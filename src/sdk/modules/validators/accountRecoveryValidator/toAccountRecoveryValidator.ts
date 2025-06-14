17a63d1aa1acb62ac75cdb2e37aa7a66c57794fd
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2025-06-14 20:22:49 +0530

    feat: acc recovery init

---
+++ src/sdk/modules/validators/accountRecoveryValidator/toAccountRecoveryValidator.ts
@@ -0,0 +1,136 @@
+import { type Address, type Hex, encodePacked } from "viem"
+import type { ModuleMeta } from "../../utils/Types"
+import type { ModularSmartAccount } from "../../utils/Types"
+import type { Module, ModuleParameters } from "../../utils/Types"
+import { toModule } from "../../utils/toModule"
+import { getSmartSessionsValidator, getSocialRecoveryMockSignature, SMART_SESSIONS_ADDRESS, SOCIAL_RECOVERY_ADDRESS } from "@rhinestone/module-sdk"
+
+// WIP
+// only makes sense in the case of single guardian right now
+
+export const DUMMY_ECDSA_SIG =
+  "0xe8b94748580ca0b4993c9a1b86b5be851bfc076ff5ce3a1ff65bf16392acfcb800f9b4f1aef1555c7fce5599fffb17e7c635502154a0333ba21f3ae491839af51c"
+
+export type AccountRecoveryModuleData = {
+    guardians: Address[]
+    threshold: number
+}
+
+export type AccountRecoveryModule = Module & {
+  moduleData?: AccountRecoveryModuleData
+}
+
+/**
+ * Arguments for getting the initialization data for a Use Session module.
+ */
+export type UsePermissionModuleGetInitDataArgs = {
+  signerAddress: Address
+}
+
+/**
+ * Parameters for creating a Use Session module.
+ */
+export type AccountRecoveryModuleParameters = Omit<
+  ModuleParameters,
+  "accountAddress" | "address"
+> & {
+  account: ModularSmartAccount
+  moduleData?: RecoveryModuleData
+}
+
+export type RecoveryModuleData = {
+    guardians: Address[]
+    threshold: number
+}
+
+/**
+ * Gets the initialization data for a Use Session module.
+ *
+ * @param _ - Optional arguments (currently unused).
+ * @returns The module metadata including address, type, and initialization data.
+ */
+export const getUsePermissionModuleInitData = (
+  _?: UsePermissionModuleGetInitDataArgs
+): ModuleMeta => ({
+  address: SOCIAL_RECOVERY_ADDRESS,
+  type: "validator",        
+  initData: "0x"
+})
+
+/**
+ * Gets the initialization data for a Use Session.
+ *
+ * @param signerAddress - The address of the signer for the session.
+ * @returns The encoded initialization data as a hexadecimal string.
+ */
+export const getUsePermissionInitData = ({
+  signerAddress
+}: UsePermissionModuleGetInitDataArgs): Hex =>
+  encodePacked(["address"], [signerAddress])
+
+/**
+ * Creates a Smart Sessions module for a modular smart account.
+ *
+ * This function sets up a Smart Sessions module with the specified parameters,
+ * including session mode, permission ID, and session data.
+ *
+ * @param parameters - The parameters for creating the Smart Sessions module.
+ * @returns A Module object representing the created Smart Sessions module.
+ *
+ * @example
+ * ```typescript
+ * const smartSessionsModule = toSmartSessionsValidator({
+ *   account: mySmartAccount,
+ *   signer: mySigner,
+ *   moduleData: {
+ *     permissionId: '0x1234...',
+ *     mode: SmartSessionMode.USE,
+ *     enableSessionData: '0x5678...'
+ *   }
+ * });
+ * ```
+ *
+ * @remarks
+ * - The function generates stub signatures and can sign user operation hashes.
+ * - It uses the SmartSession address from the predefined addresses.
+ * - The default session mode is USE if not specified.
+ */
+export const toAccountRecoveryValidator = (
+  parameters: AccountRecoveryModuleParameters
+): AccountRecoveryModule => {
+  const {
+    account,
+    signer, // could be multiple guardians and they all need to sign
+    moduleInitData: moduleInitData_,
+    deInitData = "0x",
+    initData: initData_,
+    initArgs: initArgs_ = { signerAddress: signer.address },
+    moduleData: {
+        guardians,
+        threshold = 1
+    } = {}
+  } = parameters
+
+  const initData = initData_ ?? getUsePermissionInitData(initArgs_)
+  const moduleInitData =
+    moduleInitData_ ?? getSmartSessionsValidator({ useRegistry: false })
+
+  return toModule({
+    ...parameters,
+    signer,
+    accountAddress: account.address,
+    address: SOCIAL_RECOVERY_ADDRESS,
+    initData,
+    moduleInitData,
+    deInitData,
+    getStubSignature: async () =>
+        getSocialRecoveryMockSignature({
+            threshold: threshold,
+          }),
+    signUserOpHash: async (userOpHash: Hex) => {
+        return signer.signMessage({
+            message: { raw: userOpHash as Hex }
+        })
+    }
+  }) as AccountRecoveryModule
+}
