4516a93b0546054d525a53157d88a4d837f983ba
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2026-03-26 19:44:18 +0530

    fix: remove userop status from decorators (#20)

--- dist/_esm/clients/decorators/bundler/getUserOperationStatus.js
+++
@@ -1,8 +0,0 @@
-// Review: If we could integrate our specific API
-export async function getUserOperationStatus(client, parameters) {
-    return await client.request({
-        method: "biconomy_getUserOperationStatus",
-        params: [parameters.hash]
-    });
-}
-//# sourceMappingURL=getUserOperationStatus.js.map
